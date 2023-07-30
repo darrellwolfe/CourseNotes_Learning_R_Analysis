e5abae1ad51192354645147cf8788828262b0a13
Darrell G Wolfe <darrellgwolfe@outlook.com>
2023-07-29 20:40:47 -0700

    dgw - packages!

---
+++ R_Coursera_5.R
@@ -0,0 +1,50 @@
+installed.packages()
+
+# Installing Posit Professional Drivers
+https://docs.posit.co/pro-drivers/workbench-connect/ 
+# Install Posit Professional Drivers for Windows#
+https://docs.posit.co/pro-drivers/desktop/install-method/windows/
+
+# XLConnect: Excel Connector for R
+https://cran.rstudio.com/web/packages/XLConnect/
+  https://cran.rstudio.com/web/packages/XLConnect/readme/README.html 
+
+#UserGuide
+https://cran.r-project.org/web/packages/XLConnect/XLConnect.pdf 
+
+https://github.com/r-lib/devtools.git
+
+install.packages("devtools")
+require(devtools)
+install_github("miraisolutions/xlconnect")
+
+read.csv()
+
+install.packages("tidymodels")
+library(tidymodels)
+
+install.packages("R Markdown")
+
+# ggmap
+https://journal.r-project.org/archive/2013-1/kahle-wickham.pdf
+install.packages("ggmap")
+library(ggmap)
+summarize(ggmap)
+
+
+
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
+install.packages()
