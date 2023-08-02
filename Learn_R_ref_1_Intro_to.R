0ba6c1d1b9e24f9117208134fdd0d8e272919c0a
Darrell G Wolfe <darrellgwolfe@outlook.com>
2023-08-01 23:16:00 -0700

    dgw

---
+++ Learn_R_ref_1_Intro_to.R
@@ -0,0 +1,49 @@
+# Welcome to Darrell WOlfe's R_Analysis CodeBase
+
+# I assume you are using RStudio, if not, download that now:
+  # You will want to install both:
+  R <- #That is the code and operating system information you need running in the background.
+  RStudio <- # That is the Graphic User Interface (GUI) that you will primarily interact with.
+  [Download_R_](https://posit.co/download/rstudio-desktop/) # Use: Ctrl + (Mouse Left Click)
+
+
+#  This is a basic intro to R, I will be creating the following:
+  #   Learn_R_ref_Intro_to.R <- That's where you are now
+  #   Learn_R_ref_Resources.html <- This will be links to Videos, Blogs, and Websites about R
+  #   Learn_R_ref_Basics_of.R <- This will be covering the very basic elements of R syntax, vocabulary, etc.
+  #   Learn_R_ref_HowTo_TBC <- 
+  #   Learn_R_ref_TBC <- 
+  #   Learn_R_ref_TBC <- 
+
+# I'm creating this as I'm learning R through Coursera (below)
+  # LINK: to open, Ctrl+Left-Click-With-Mouse
+    [Data Analysis with R Programming](https://www.coursera.org/learn/data-analysis-r/home/welcome)
+    # OR this one
+    shiny::a(href = "https://www.coursera.org/learn/data-analysis-r/home/welcome", "Data Analysis with R Programming")
+
+# BACKGROUND OF R per GPT-4:
+  # The "R" in R programming doesn't officially stand for anything. 
+  # The language was named "R" by its creators, Ross Ihaka and Robert Gentleman, 
+  #   from the University of Auckland, New Zealand. It can be viewed as a play on their names' initial letter. 
+  
+  # However, the language is also considered as an implementation of the S programming language, 
+  #   so sometimes "R" is referred to as a sort of play on the name "S". 
+  # S was created by John Chambers and colleagues at Bell Laboratories.
+  
+  # R has become a go-to language for statistical computing and graphics, widely used among statisticians and data analysts.
+
+# SYSTEM INFORMATION  
+  #To run code, place cursor in the same line as the code, press Ctrl+Enter (or, click "Run" icon at the top right or this box)
+  
+  # To see which version of RStudio you are on, place cursor in the same line as the code, press Ctrl+Enter:
+  version
+  
+  # To see which packages are already installed, place cursor in the same line as the code, press Ctrl+Enter:
+  installed.packages()
+  
+  # To install packages, place cursor in the same line as the code, press Ctrl+Enter:
+  install.packages("tidyverse")
+  
+  # To load the library from installed packages, place cursor in the same line as the code, press Ctrl+Enter:
+  library(tidyverse)
+  
---
+++ Learn_R_ref_3_Basics_of.R
@@ -0,0 +1,7 @@
+# <- That is a comment marker, R ignores Comments #, ##, ###,
+
+# This text-box-area you're reading in right now is
+
+
+print("Hello, World!")
+#To run a command, place your curser in the line of code (or highlight if multiple lines) and press Ctrl+Enter (or Command+Enter for Mac)
