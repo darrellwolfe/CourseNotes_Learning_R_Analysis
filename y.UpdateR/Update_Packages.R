3e9ca6ddc1bd223ee9339804bf8d5262182cc869
Darrell G Wolfe <darrellgwolfe@outlook.com>
2023-08-08 16:21:27 -0700

    dgw

---
+++ y.UpdateR/Update_Packages.R
@@ -0,0 +1,10 @@
+# In order to update R to the latest version, you have to reinstall. See the Learn_R User Guide. 
+
+# I need the following packages (to be updated periodically)
+
+```{r echo = true}
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("shiny")
+```
+
