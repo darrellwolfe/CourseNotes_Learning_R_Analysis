2ba7c35ba5def2fe6436dff190b095c1e4523f7f
Darrell G Wolfe <darrellgwolfe@outlook.com>
2023-08-22 16:08:03 -0700

    dgw

---
+++ SQL/dbo.Cyclist.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Cyclistic_divvy_tripdata] (
+    ride_id VARCHAR(16) PRIMARY KEY,
+    rideable_type VARCHAR(15),
+    started_at DATETIME,
+    ended_at DATETIME,
+    start_station_name VARCHAR(50),
+    start_station_id VARCHAR(50),
+    end_station_name VARCHAR(50),
+    end_station_id VARCHAR(50),
+    start_lat DECIMAL(5, 2),
+    start_lng DECIMAL(5, 2),
+    end_lat DECIMAL(5, 2),
+    end_lng DECIMAL(5, 2),
+    member_casual VARCHAR(10)
+);
+
+
